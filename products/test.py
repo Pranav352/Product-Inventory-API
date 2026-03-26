c6a711ac944e155ecbb254b713101873901f04c4
Pranav Patel <pranav742001@gmail.com>
2026-03-26 18:18:37 +0530

    add test

---
+++ products/test.py
@@ -0,0 +1,17 @@
+# from django.test import TestCase
+# from faker import Faker
+# from models import*
+
+# class FirstTestcase(TestCase):
+
+#     def setup(self):
+#         print('setup called')
+
+#     def test_equal(self):
+#         self.assertEqual(1 , 1)
+
+
+
+
+
+
---
+++ test/test_product_model.py
@@ -0,0 +1,35 @@
+
+
+import pytest
+from products.models import Product
+
+@pytest.mark.django_db
+
+def test_product_creation():
+    product = Product.objects.create(
+        name="Laptop",
+        price=50000,
+        quentity=10
+    )
+
+    assert product.name == "Laptop"
+    assert product.price == 50000
+    assert product.quentity == 10
+
+
+@pytest.mark.django_db
+def test_product_str_method():
+        product = Product.objects.create(
+            name="Phone",
+            price=50000,
+            quentity=100
+    )
+        assert str(product) == "Phone"
+@pytest.mark.django_db
+def test_created_at_field():
+    product = Product.objects.create(
+        name="Tablet",
+        price=30000,
+        quentity=20
+    )
+    assert product.created_at is not None
